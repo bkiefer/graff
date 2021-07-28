551a75a4e85bbaade34968c6592b2d09ab11fc29
Bernd Kiefer <kiefer@semvox.de>
2021-07-28 10:09:05 +0200

    Adapt license

--- src/main/java/de/dfki/lt/loot/util/Predicates.java
+++ src/main/java/de/dfki/lt/loot/util/Predicates.java
@@ -1,8 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2019-2022 Jörg Steffen, Bernd Kiefer
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * ​https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
+
 package de.dfki.lt.loot.util;
 
 import java.util.Comparator;
@@ -30,7 +41,8 @@ public class Predicates {
      * @param arg2 a <code>ArgumentType</code> with the second argument
      * @return a <code>boolean</code> indicating if the arguments are compatible
      */
-    public boolean test(ArgumentType arg1, ArgumentType arg2) {
+    @Override
+	public boolean test(ArgumentType arg1, ArgumentType arg2) {
 
       return arg1.equals(arg2);
     }
@@ -52,7 +64,8 @@ public class Predicates {
      * @param arg2 a <code>ArgumentType</code> with the second argument
      * @return a <code>boolean</code> indicating if the arguments are compatible
      */
-    public boolean test(ArgumentType arg1, ArgumentType arg2) {
+    @Override
+	public boolean test(ArgumentType arg1, ArgumentType arg2) {
 
       return arg1.compareTo(arg2) == 0;
     }
@@ -74,7 +87,8 @@ public class Predicates {
      * @param arg2 a <code>ArgumentType</code> with the second argument
      * @return a <code>boolean</code> indicating if the arguments are compatible
      */
-    public int compare(ArgumentType arg1, ArgumentType arg2) {
+    @Override
+	public int compare(ArgumentType arg1, ArgumentType arg2) {
 
       return arg1.compareTo(arg2);
     }
